17b4aef58b43e8a5501322aa85d590147c81b50f
KigenEugin <102093163+KigenEugin@users.noreply.github.com>
2022-03-22 03:23:35 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int action;
+printf("select an action\n");
+printf("1. Add new patron\n");
+printf("2. View patron\n");
+printf("3. View books\n");
+printf("4. Add new books\n");
+printf("Your actions");
+scanf("%d",&action);
+
+return 0;
+}
