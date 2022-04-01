4aa88aa215d30828111c5de9d3f2125c4b5b2866
KigenEugin <102093163+KigenEugin@users.noreply.github.com>
2022-04-01 20:35:01 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,13 +4,20 @@
 int main()
 {
 int action;
+
 printf("select an action\n");
 printf("1. Add new patron\n");
 printf("2. View patron\n");
 printf("3. View books\n");
 printf("4. Add new books\n");
-printf("Your actions");
+printf("Your actions ");
 scanf("%d",&action);
 
-return 0;
+return action;
+}
+int menu()
+{
+    printf("county library system!\n");
+    printf("welcome Eugene!\n");
+    return 0 ;
 }
